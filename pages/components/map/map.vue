e5b99ec640b40cd9ade806597f4aff3b4274553a
sunqi <zqbysq@163.com>
2020-09-24 16:23:51 +0800

    update

---
+++ pages/components/map/map.vue
@@ -0,0 +1,29 @@
+<template>
+	<u-action-sheet :list="list" v-model="show"></u-action-sheet>
+	<canvas canvas-id="map" style="328px;height:582px;"></canvas>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: [{
+					text: '点赞',
+					color: 'blue',
+					fontSize: 28
+				}, {
+					text: '分享'
+				}, {
+					text: '评论'
+				}],
+				show: true
+			}
+		},
+		mounted() {
+
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ pages/setting/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<view v-for="(item,index) in dataList" :key="index">
+		
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			dataList:[]
+		}
+	}
+</script>
+
+<style>
+</style>
